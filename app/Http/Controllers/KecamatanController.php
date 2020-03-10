d278402abf48ad6bbe322b220d21efc4610cbf98
acek27 <razak.syaiful.r@gmail.com>
2020-03-10 15:06:05 +0700

    new assets

---
+++ app/Http/Controllers/KecamatanController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Traits\Resource;
+
+class KecamatanController extends Controller
+{
+    use Resource;
+//    protected $model = Aspek::class;
+    protected $view = 'kecamatans';
+    protected $route = 'kecamatan';
+
+    public function __construct()
+    {
+        $this->middleware('can:admin');
+    }
+}
